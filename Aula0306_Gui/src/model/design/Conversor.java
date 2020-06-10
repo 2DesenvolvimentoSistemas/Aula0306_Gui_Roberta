cdbd9e534f2b8e4d221123ef6b78c922781ebb31
ANTONIO <ANTONIO@10.0.0.105>
2020-06-10 19:45:59 -0300

    Conversor de temperatura com interface gráfica (padrão MVC)- commit Roberta

---
+++ Aula0306_Gui/src/model/design/Conversor.java
@@ -0,0 +1,17 @@
+
+package model.design;
+
+/**
+ *
+ * @author ROBERTA
+ */
+public class Conversor {
+    
+    //    metodo que converte de °C para °F e formula
+    public double celFah (double cel){
+        return 9 * cel / 5 + 32;
+                
+     
+    }
+    
+}
---
+++ Aula0306_Gui/src/model/teste/TesteConversor.java
@@ -0,0 +1,26 @@
+
+package model.teste;
+
+import model.design.Conversor;
+        
+/**
+ *
+ * @author ROBERTA  
+ */
+public class TesteConversor {
+    
+//    criacao do metodo main
+    public static void main(String[] args) {
+        
+//        criacao e instancia de objeto da classe conversor
+            Conversor c = new Conversor ();
+        
+//     atribuindo dois numeros depois da virgula e metodos
+        System.out.println("\n\t\t\t -- CONVERSOR DE °C PARA °F \n");
+        System.out.printf("%.2f°C = %.2f°F\n", 0.0, c.celFah(00.00));
+        System.out.printf("%.2f°C = %.2f°F\n", -40.0, c.celFah(-40.0));
+        System.out.printf("%.2f°C = %.2f°F\n", 38.0, c.celFah(38.0));
+    }
+    
+}
+

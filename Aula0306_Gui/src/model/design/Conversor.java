94941df596afc815813ec0f6ca9afd527f065777
ANTONIO <ANTONIO@10.0.0.105>
2020-06-10 22:07:03 -0300

    metodo que converte de °F para °C e formula-commit Roberta

--- Aula0306_Gui/src/model/design/Conversor.java
+++ Aula0306_Gui/src/model/design/Conversor.java
@@ -7,11 +7,15 @@ package model.design;
  */
 public class Conversor {
     
-    //    metodo que converte de °C para °F e formula
+ //    metodo que converte de °C para °F e formula
     public double celFah (double cel){
         return 9 * cel / 5 + 32;
                 
      
+    }
+//    metodo que converte de °F para °C e formula
+    public double fahCel (double fah){
+        return (fah - 32) / 9 * 5;
     }
     
 }

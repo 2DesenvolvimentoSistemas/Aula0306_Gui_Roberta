ed61f597176175488f1f2c6c84fd1b89d7377bd6
ANTONIO <ANTONIO@10.0.0.105>
2020-06-10 22:04:17 -0300

    sout de °F para °C kkk - commit Roberta

--- Aula0306_Gui/src/model/teste/TesteConversor.java
+++ Aula0306_Gui/src/model/teste/TesteConversor.java
@@ -20,6 +20,12 @@ public class TesteConversor {
         System.out.printf("%.2f°C = %.2f°F\n", 0.0, c.celFah(00.00));
         System.out.printf("%.2f°C = %.2f°F\n", -40.0, c.celFah(-40.0));
         System.out.printf("%.2f°C = %.2f°F\n", 38.0, c.celFah(38.0));
+        
+//        conversor de °F para °C
+            System.out.println("\n\t\t\t -- CONVERSOR DE °F PARA °C \n");
+            System.out.printf("%.2f°C = %.2f°F\n", 32.0, c.fahCel(32.0));
+            System.out.printf("%.2f°C = %.2f°F\n", -40.0, c.fahCel(-40.0));
+            System.out.printf("%.2f°C = %.2f°F\n", 100.4, c.fahCel(100.4));
     }
     
 }
